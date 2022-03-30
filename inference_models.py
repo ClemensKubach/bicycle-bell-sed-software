739787df51d46bc75af7c737b63edd73a38f0ca7
ClemensKubach <clemens.kubach@gmail.com>
2022-03-30 21:12:14 +0200

    add model classes

---
+++ inference_models.py
@@ -0,0 +1,78 @@
+"""Inference Models"""
+import logging
+from abc import ABC, abstractmethod
+
+import tensorflow as tf
+
+from saved_models import BaseSavedModel
+
+
+class BaseInferenceModel(ABC):
+    """Base Inference Model"""
+
+    def __init__(self, saved_model: BaseSavedModel) -> None:
+        self.logger = logging.getLogger(__name__)
+        self.saved_model = saved_model
+        self._convert_model()
+        self._prepare_interpreter()
+
+    @property
+    @abstractmethod
+    def _converted_model_path(self) -> str:
+        """path to converted model"""
+
+    @abstractmethod
+    def _convert_model(self):
+        """converts Model into inference model"""
+
+    @abstractmethod
+    def _prepare_interpreter(self):
+        """prepare inference model interpreter"""
+
+    @abstractmethod
+    def _predict(self, preprocessed_sample) -> tf.Tensor:
+        """predict with model interpreter. returns tensor with prediction result for one sample,
+        not batch. """
+
+    def inference(self, sample: tf.Tensor, sample_rate: int):
+        """inference"""
+        preprocessed_sample = self.saved_model.preprocess(sample, sample_rate)
+        inference_result = self._predict(preprocessed_sample)
+        prob, label = self.saved_model.extract_prediction(inference_result)
+        return prob, label
+
+
+class TFLiteInferenceModel(BaseInferenceModel):
+    """TFLite Inference Model"""
+
+    @property
+    def _converted_model_path(self) -> str:
+        return './model/bicycle_bell_model.tflite'
+
+    def _convert_model(self):
+        converter = tf.lite.TFLiteConverter.from_saved_model(self.saved_model.saved_model_path)
+        converter.experimental_new_converter = True
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
+                                               tf.lite.OpsSet.SELECT_TF_OPS]
+        tflite_model = converter.convert()
+        with open(self._converted_model_path, 'wb') as file:
+            file.write(tflite_model)
+
+    def _prepare_interpreter(self):
+        # Load the TFLite model and allocate tensors.
+        self.interpreter = tf.lite.Interpreter(self._converted_model_path)
+        self.interpreter.allocate_tensors()
+        # Get input and output tensors.
+        self.tensor_input_details = self.interpreter.get_input_details()
+        self.logger.debug(f"Input Details: {str(self.tensor_input_details)}")
+        self.tensor_output_details = self.interpreter.get_output_details()
+        input_shape = self.tensor_input_details[0]['shape']
+        self.logger.debug(f"Input Shape: {str(input_shape)}")
+
+    def _predict(self, preprocessed_sample) -> tf.Tensor:
+        self.interpreter.set_tensor(self.tensor_input_details[0]['index'], preprocessed_sample)
+        self.logger.debug("invoke prediction")
+        self.interpreter.invoke()
+        # = [[y_pred_prob]] shape=(batch_size, pred)
+        result_tensor = self.interpreter.tensor(self.tensor_output_details[0]['index'])[0]
+        return result_tensor
---
+++ saved_models.py
@@ -0,0 +1,77 @@
+"""TF saved Models"""
+
+import logging
+from abc import ABC, abstractmethod
+
+import tensorflow as tf
+import tensorflow_io as tfio
+
+
+class BaseSavedModel(ABC):
+    """ Base Model"""
+
+    @abstractmethod
+    def __init__(self, saved_model_path: str, threshold: float) -> None:
+        self.logger = logging.getLogger(__name__)
+        self.logger.info(
+            f'Loading model {self._name} from: {saved_model_path} with threshold: {threshold}'
+        )
+        self.saved_model_path = saved_model_path
+        self.threshold = threshold
+
+    @property
+    @abstractmethod
+    def _name(self) -> str:
+        """name of the model"""
+
+    @abstractmethod
+    def preprocess(self, sample: tf.Tensor, data_sample_rate: int) -> tf.Tensor:
+        """preprocess"""
+
+    @abstractmethod
+    def extract_prediction(self, sample_prediction: tf.Tensor) -> tuple:
+        """predict"""
+
+
+class Mono16kWaveInputSavedModel(BaseSavedModel, ABC):
+    """Mono16kWaveInput"""
+
+    def __init__(self, saved_model_path: str, threshold: float) -> None:
+        super().__init__(saved_model_path, threshold)
+        self.sample_rate = 16000
+
+    def preprocess(self, sample: tf.Tensor, data_sample_rate: int) -> tf.Tensor:
+        if not isinstance(sample, tf.Tensor) or tf.rank(sample) != 1:
+            raise ValueError('Sample must be a 1D Tensor')
+        if int(data_sample_rate) != self.sample_rate:
+            sample = tfio.audio.resample(sample, data_sample_rate, self.sample_rate)
+        return sample
+
+    def extract_prediction(self, sample_prediction: tf.Tensor) -> tuple:
+        y_pred_prob = sample_prediction[0]  # = [[y_pred_prob]] shape=(batch_size, pred)
+        y_pred_label = bool(y_pred_prob > self.threshold)
+        return y_pred_prob, y_pred_label
+
+
+class CrnnSavedModel(Mono16kWaveInputSavedModel):
+    """CrnnSavedModel"""
+
+    @property
+    def _name(self) -> str:
+        return 'crnn'
+
+
+class YamNetBaseSavedModel(Mono16kWaveInputSavedModel):
+    """YamNetBaseSavedModel"""
+
+    @property
+    def _name(self) -> str:
+        return 'yamnet-base'
+
+
+class YamNetExtendedSavedModel(Mono16kWaveInputSavedModel):
+    """YamNetExtendedSavedModel"""
+
+    @property
+    def _name(self) -> str:
+        return 'yamnet-extended'
